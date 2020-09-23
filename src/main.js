f22024108edf864a9d719cc1c1b5017f1deacd8b
Alen Alic <aalic89@gmail.com>
2020-09-23 12:29:41 +0200

    added => force websocket connection

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ import toast from "./plugins/toast";
 Vue.use(toast);
 import VueSocketIOExt from "vue-socket.io-extended";
 import io from "socket.io-client";
-Vue.use(VueSocketIOExt, io(process.env.SOCKET_URL, { autoConnect: false }));
+Vue.use(VueSocketIOExt, io(process.env.SOCKET_URL, { autoConnect: false, transports: ["websocket"] }));
 
 // External flag css
 import "flag-icon-css/css/flag-icon.min.css";
